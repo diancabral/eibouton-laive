966146ee92f1a4d3f1b7ba98bcac9e7c76f88c5f
Dian Carlos <dian.cabral@gmail.com>
2022-07-26 17:37:14 -0300

    develop

--- src/components/ui/MIDIIndicator/MIDIIndicator.tsx
+++ src/components/ui/MIDIIndicator/MIDIIndicator.tsx
@@ -4,9 +4,9 @@ import { MIDIInputType } from '../../../store/midi/types';
 import * as Styled from './styled';
 
 type MIDIIndicatorType = {
-  data: MIDIInputType
-  small?: boolean
-}
+  data: MIDIInputType;
+  small?: boolean;
+};
 
 export const MIDIIndicator = ({ data, small }: MIDIIndicatorType) => {
   const midi = useMemo(() => data.notesOn, [data.notesOn]);
@@ -14,15 +14,19 @@ export const MIDIIndicator = ({ data, small }: MIDIIndicatorType) => {
     const length = small ? 10 : 20;
     const array = [...Array(length).keys()];
     const note = midi.slice(-1)[0];
-    return array.map((val, index) => ({
-      active: !!midi.length && (index * (127 / length) <= (note.velocity || 0)),
-      duration: (!!midi.length ? index : array.length - index) / 100
-    })).reverse();
+    return array
+      .map((val, index) => ({
+        active: !!midi.length && index * (127 / length) <= (note.velocity || 0),
+        duration: (!!midi.length ? index : array.length - index) / 200,
+      }))
+      .reverse();
   }, [midi]);
 
   return (
     <Styled.Container>
-      { dots.map((val, index) => <Styled.Dots $active={val.active} $duration={val.duration} key={index} $small={small as boolean} />)}
+      {dots.map((val, index) => (
+        <Styled.Dots $active={val.active} $duration={val.duration} key={index} $small={small as boolean} />
+      ))}
     </Styled.Container>
-  )
+  );
 };
