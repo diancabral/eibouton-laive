cf9933943f00a7a897e816be5d7bc06ce5d551bd
Dian Carlos <dian.cabral@gmail.com>
2022-08-03 13:59:26 -0300

    develop

--- src/devices/Merus/MerusInterface.tsx
+++ src/devices/Merus/MerusInterface.tsx
@@ -44,60 +44,58 @@ export const MerusInterface = ({ data }: MerusInterfaceProps) => {
   };
 
   return (
-    <Styled.Container>
-      <Wrapper theme="mid-dark" direction="column">
-        <FormSelect
-          label="Wave"
-          options={[
-            {
-              label: 'Sawtooth',
-              value: 'sawtooth',
-            },
-            {
-              label: 'Square',
-              value: 'square',
-            },
-            {
-              label: 'Sine',
-              value: 'sine',
-            },
-            {
-              label: 'Triangle',
-              value: 'triangle',
-            },
-          ]}
-          value={device.config.wave || 'sawtooth'}
-          onSelect={(e) => handleSelectedWave(e)}
-        />
-        <FormLabel>Octave</FormLabel>
-        <Knob theme="black" min={-4} max={4} onChange={(e) => false} />
-        <FormLabel>Fine</FormLabel>
-        <Knob theme="black" min={-100} max={100} onChange={(e) => false} />
-      </Wrapper>
-      <Wrapper theme="dark" direction="column">
-        <Styled.Container>
-          <Wrapper theme="black">
-            <FormLabel>Amp Envelope</FormLabel>
-            <ADSR width={250} height={90} attack={device.config.attack} decay={device.config.decay} sustain={device.config.sustain} release={device.config.release} midi={midi} />
-            {/* <input type="number" value={attack} onChange={(e) => setAttack(parseFloat(e.target.value))} />
-            <input type="number" value={decay} onChange={(e) => setDecay(parseFloat(e.target.value))} /> */}
-            <Styled.Container
-              style={{
-                marginTop: '10px',
-              }}
-            >
-              <FormLabel>Attack</FormLabel>
-              <Knob theme="light" min={0} max={32000} value={device.config.attack} onChange={(e) => handleADSR(e, 'attack')} step={milisecondsKnobStep} format={formatMiliseconds} />
-              <FormLabel>Decay</FormLabel>
-              <Knob theme="light" min={0} max={32000} value={device.config.decay} onChange={(e) => handleADSR(e, 'decay')} step={milisecondsKnobStep} format={formatMiliseconds} />
-              <FormLabel>Sustain</FormLabel>
-              <Knob theme="light" min={0} max={100} step={(e) => 1} format={(e) => e.toFixed(1)} value={device.config.sustain} onChange={(e) => handleADSR(e, 'sustain')} />
-              <FormLabel>Release</FormLabel>
-              <Knob theme="light" min={0} max={32000} value={device.config.release} onChange={(e) => handleADSR(e, 'release')} step={milisecondsKnobStep} format={formatMiliseconds} />
-            </Styled.Container>
-          </Wrapper>
-        </Styled.Container>
-      </Wrapper>
-    </Styled.Container>
+    <Wrapper theme="dark" direction="row">
+      <Styled.Container>
+        <Wrapper theme="mid-dark" direction="column">
+          <FormSelect
+            label="Wave"
+            options={[
+              {
+                label: 'Sawtooth',
+                value: 'sawtooth',
+              },
+              {
+                label: 'Square',
+                value: 'square',
+              },
+              {
+                label: 'Sine',
+                value: 'sine',
+              },
+              {
+                label: 'Triangle',
+                value: 'triangle',
+              },
+            ]}
+            value={device.config.wave || 'sawtooth'}
+            onSelect={(e) => handleSelectedWave(e)}
+          />
+          <FormLabel>Octave</FormLabel>
+          <Knob theme="light" min={-4} max={4} onChange={(e) => false} />
+          <FormLabel>Fine</FormLabel>
+          <Knob theme="light" min={-100} max={100} onChange={(e) => false} />
+        </Wrapper>
+        <Wrapper theme="black">
+          <FormLabel>Amp Envelope</FormLabel>
+          <ADSR width={250} height={90} attack={device.config.attack} decay={device.config.decay} sustain={device.config.sustain} release={device.config.release} midi={midi} />
+          {/* <input type="number" value={attack} onChange={(e) => setAttack(parseFloat(e.target.value))} />
+        <input type="number" value={decay} onChange={(e) => setDecay(parseFloat(e.target.value))} /> */}
+          <Styled.Container
+            style={{
+              marginTop: '10px',
+            }}
+          >
+            <FormLabel>Attack</FormLabel>
+            <Knob theme="light" min={0} max={32000} value={device.config.attack} onChange={(e) => handleADSR(e, 'attack')} step={milisecondsKnobStep} format={formatMiliseconds} />
+            <FormLabel>Decay</FormLabel>
+            <Knob theme="light" min={0} max={32000} value={device.config.decay} onChange={(e) => handleADSR(e, 'decay')} step={milisecondsKnobStep} format={formatMiliseconds} />
+            <FormLabel>Sustain</FormLabel>
+            <Knob theme="light" min={0} max={100} step={(e) => 1} format={(e) => e.toFixed(1)} value={device.config.sustain} onChange={(e) => handleADSR(e, 'sustain')} />
+            <FormLabel>Release</FormLabel>
+            <Knob theme="light" min={0} max={32000} value={device.config.release} onChange={(e) => handleADSR(e, 'release')} step={milisecondsKnobStep} format={formatMiliseconds} />
+          </Styled.Container>
+        </Wrapper>
+      </Styled.Container>
+    </Wrapper>
   );
 };
